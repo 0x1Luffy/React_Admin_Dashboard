1af4ba271c821dee7d838d77aba5697772a720e5
0x1Luffy <chetankesare06@gmail.com>
2024-01-04 01:19:37 +0530

    added react router dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import Home from "./pages/home/Home"
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Users from "./pages/users/Users";
+import Product from "./pages/product/Product";
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:
+        <Home/>,
+    },
+    {
+      path: "users",
+      element: <Users/>,
+    },
+    {
+      path: "product",
+      element: <Product/>,
+    },
+  ]);
+
   return (
-    <div><Home/></div>
+   <RouterProvider router={router}/>
   )
 }
 
