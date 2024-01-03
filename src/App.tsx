0c76d934b363544e5bf39f1d1016e3b364b6f2ea
0x1Luffy <chetankesare06@gmail.com>
2024-01-04 01:47:12 +0530

    created components, defined routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,50 @@ import Home from "./pages/home/Home"
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
+  Outlet
 } from "react-router-dom";
 import Users from "./pages/users/Users";
 import Product from "./pages/product/Product";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
 function App() {
 
+
+  const  Layout = ()=>{
+    return (
+      <div className="main">
+        <Navbar/>
+        <div className="container">
+          <div className="menuContainer">
+            <Menu/>
+          </div>
+          <div className="contentContainer">
+            <Outlet/>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    );
+  };
   const router = createBrowserRouter([
     {
-      path: "/",
-      element:
-        <Home/>,
-    },
-    {
-      path: "users",
-      element: <Users/>,
-    },
-    {
-      path: "product",
-      element: <Product/>,
+      path:"/",
+      element:<Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path:"/users",
+          element:<Users/>
+        },
+        {
+          path:"/products",
+          element:<Product/>
+        },
+      ]
     },
   ]);
 
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div className='menu'>Navbar</div>
+  )
+}
+
+export default Menu
