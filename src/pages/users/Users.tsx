74f51724eead5c2aaeedd2ded88dfd37391d9225
0x1Luffy <chetankesare06@gmail.com>
2024-01-06 20:34:01 +0530

    Installed MUI Data grid!

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,13 @@
 import "./users.scss"
 const Users = () => {
   return (
-    <div className="home">Users</div>
+    <div className="users">
+      <div className="info">  
+      <h1>Users</h1>
+      <button>Add New User</button>
+      </div>
+       
+    </div>
   )
 }
 
