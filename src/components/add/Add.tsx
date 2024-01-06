ed48d5c41df702932127efb0a44bc55f4e5dcd6c
0x1Luffy <chetankesare06@gmail.com>
2024-01-06 23:32:44 +0530

    Started Styling the ADD User Modal!

--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -6,10 +6,30 @@ type Props = {
      columns:GridColDef[]
      setOpen:React.Dispatch<React.SetStateAction<boolean>>;
 }
-const Add = () => {
-  return (
+const Add = (props:Props) => {
+
+    const handleSubmit=(e: React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+
+
+    }
+  return ( 
     <div className="add">
-        <div className="modal"></div>
+        <div className="modal">
+            <span className="close" onClick={()=>props.setOpen(false)}>X</span>
+            <h1>Add New {props.slug}</h1>
+            <form onSubmit={handleSubmit}>
+                {props.columns.filter(item=>item.field !=="id" && item.field !== "img")
+                .map(column=>(
+                    <div className="item">
+                        <label>{column.headerName}</label>
+                        <input type={column.type} placeholder={column.field} />
+                    </div>
+                ))}
+
+                <button>Send</button>
+            </form>
+        </div>
     </div>
   )
 }
