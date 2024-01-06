cc538ff2ae519efdd5f69658c1a4a3ba954b9d24
0x1Luffy <chetankesare06@gmail.com>
2024-01-06 15:01:36 +0530

    removed defaut styling of the charts.

--- src/components/chartBox/ChartBox.tsx
+++ src/components/chartBox/ChartBox.tsx
@@ -71,7 +71,12 @@ const ChartBox = () => {
             <div className="chart">
             <ResponsiveContainer width="100%" height="100%">
         <LineChart width={300} height={100} data={data}>
-            <Tooltip/>
+            <Tooltip
+            contentStyle={{background:"transparent", border:"none"}}
+            labelStyle={{display:"none"}}
+            position={{x:10,y:60}}
+            
+            />
           <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} dot={false} />
         </LineChart>
       </ResponsiveContainer>
