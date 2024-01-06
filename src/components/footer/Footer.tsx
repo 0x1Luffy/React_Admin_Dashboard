771e443855b590d27200a53302e8ad3561ff7d58
0x1Luffy <chetankesare06@gmail.com>
2024-01-06 12:43:12 +0530

    designed basic navbar and footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,10 @@
-
+import "./footer.scss"
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <span>DashReact</span>
+      <span>Admin Dash Board </span>
+    </div>
   )
 }
 
