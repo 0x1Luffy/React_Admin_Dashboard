1827c97a7d3bba7f197bc93f03a208634db52c1f
0x1Luffy <chetankesare06@gmail.com>
2024-01-07 07:58:14 +0530

    Started Buiding Add Products Page!

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,8 +1,20 @@
-import "./product.scss"
+import { useState } from "react";
+import "./product.scss";
+import DataTable from "../../components/DataTable/DataTable";
+import Add from "../../components/add/Add";
 const Product = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="product">Product</div>
-  )
+    <div className="products">
+      <div className="info">
+        <h1>Users</h1>
+        <button onClick={() => setOpen(true)}>Add New Product</button>
+      </div>
+      <DataTable slug="products" columns={columns} rows={userRows} />
+      {open && <Add slug="product" columns={columns} setOpen={setOpen} />}
+    </div>
+  );
 }
 
 export default Product
